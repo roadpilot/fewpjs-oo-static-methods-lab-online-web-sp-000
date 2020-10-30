e8273a80c43dbc28852fa2411f278eb6e1042f30
mdacht777 <mdacht777@gmail.com>
2020-10-30 16:20:56 -0600

    commit tree 0a54599b74adbb1805d56e50dfd0b14d4d75be58

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string){
+    return string.replace(/[^0-9a-z-' ]/gi, '')
+  }
+  static titleize(string){
+    const skipwords = new Set(['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'])
+    let words = string.split(' ')
+    for (let i=0; i<words.length; i++){
+      if (!skipwords.has(words[i].toLowerCase()) || i==0){
+        words[i] = this.capitalize(words[i])
+      }
+      // console.log(words.join(' '))
+    }
+      return(words.join(' '))
+    
+  }
 }
